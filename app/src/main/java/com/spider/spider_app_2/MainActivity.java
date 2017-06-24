bf5453ab5b4ba37d5d8efa227a6f6311b36746b6
Arunimad <rarunrd@gmail.com>
2017-06-24 20:38:35 +0530

    Final commit, Hacker Mode( with 10 sec delay)

---
+++ app/src/main/java/com/spider/spider_app_2/MainActivity.java
@@ -0,0 +1,138 @@
+package com.spider.spider_app_2;
+
+import android.hardware.Sensor;
+import android.hardware.SensorEvent;
+import android.hardware.SensorEventListener;
+import android.hardware.SensorManager;
+import android.media.Ringtone;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.CountDownTimer;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.widget.TextView;
+import android.widget.Toast;
+
+
+public class MainActivity extends AppCompatActivity implements SensorEventListener{
+
+
+    private SensorManager senman;
+    private Sensor sen;
+    TextView text;
+    TextView text2;
+
+    Uri notification;
+    Ringtone r;
+    int timee =10;
+
+
+
+
+
+
+
+
+
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+
+
+
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+         text = (TextView) findViewById(R.id.text1);
+        text2 = (TextView) findViewById(R.id.text2);
+
+        senman = (SensorManager) getSystemService(SENSOR_SERVICE);
+        sen = senman.getDefaultSensor(Sensor.TYPE_PROXIMITY);
+
+        notification = RingtoneManager.getDefaultUri(RingtoneManager.TYPE_NOTIFICATION);
+        r = RingtoneManager.getRingtone(getApplicationContext(),notification);
+
+
+
+
+
+
+
+
+
+    }
+
+    protected void onResume(){
+
+        super.onResume();
+
+         senman.registerListener(this,sen,SensorManager.SENSOR_DELAY_NORMAL);
+
+
+
+    }
+
+    protected void onPause(){
+        super.onPause();
+        senman.unregisterListener(this);
+    }
+
+
+
+    public void onSensorChanged(SensorEvent event){
+
+
+
+
+
+
+        if(event.values[0] == 0){
+
+
+
+
+            text.setText("Near");
+            try {
+                Thread.sleep(10000);
+            } catch (InterruptedException e) {
+                e.printStackTrace();
+            }
+
+            text.setText("Near");
+
+
+            r.play();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        }
+        else
+
+            text.setText("Far");
+
+
+
+
+    }
+
+    @Override
+    public void onAccuracyChanged(Sensor sensor, int accuracy) {
+
+    }
+
+
+}
